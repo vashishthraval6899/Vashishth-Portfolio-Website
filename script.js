1e9bbde3b1d372dfc633ee258d418429d2345b35
Vashishth Raval <vraval765@gmail.com>
2026-01-22 19:41:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  document.querySelector('.mobile-menu')?.classList.toggle('open');
 }
